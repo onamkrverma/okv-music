69874a835e56209446685efb4428ec9af1e3c138
onam <onamkrverma@gmail.com>
2023-03-21 13:39:26 +0530

    Refactor code

--- src/page/home/heroBanner/HeroBanner.js
+++ src/page/home/heroBanner/HeroBanner.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import './HeroBanner.css';
-import Slider from 'react-slick';
-import NextArrow from './NextArrow';
-import PrevArrow from './PrevArrow';
-import HeroBannerSkeleton from './HeroBannerSkeleton';
-import { useDispatch } from 'react-redux';
-import { addSongInfo } from '../../../reduxtool/slice/currentSongSlice';
+import React from "react";
+import "./HeroBanner.css";
+import Slider from "react-slick";
+import NextArrow from "./NextArrow";
+import PrevArrow from "./PrevArrow";
+import HeroBannerSkeleton from "./HeroBannerSkeleton";
+import { useDispatch } from "react-redux";
+import { addSongInfo } from "../../../reduxtool/slice/currentSongSlice";
 
 const HeroBanner = ({ trendingSongs, isLoading }) => {
- const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const settings = {
     dots: false,
@@ -20,44 +20,56 @@ const HeroBanner = ({ trendingSongs, isLoading }) => {
     autoplaySpeed: 4000,
     cssEase: "linear",
     nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 426,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   };
 
-
-
   const handleRedirect = (videoId) => {
-    dispatch(addSongInfo(
-      {
+    dispatch(
+      addSongInfo({
         id: videoId,
-        onMiniPlayer: false
-      }
-    ))
-  }
-
+        onMiniPlayer: false,
+      })
+    );
+  };
 
   return (
-    <div className='heroBanner-section container'>
-      {isLoading ? <HeroBannerSkeleton />
-        :
+    <div className="heroBanner-section container">
+      {isLoading ? (
+        <HeroBannerSkeleton />
+      ) : (
         <Slider {...settings}>
-          {trendingSongs?.slice(0, 5).map((songs) =>
-            <div className="heroBanner-wrapper " key={songs.etag} onClick={() => handleRedirect(songs.snippet.resourceId.videoId)}>
+          {trendingSongs?.slice(0, 5).map((songs) => (
+            <div
+              className="heroBanner-wrapper "
+              key={songs.etag}
+              onClick={() => handleRedirect(songs.snippet.resourceId.videoId)}
+            >
               <div className="hero-image-wrapper">
                 <img
-                  src={songs.snippet.thumbnails?.maxres ? songs.snippet?.thumbnails.maxres?.url 
-                  : songs.snippet?.thumbnails.high?.url}
-                  className='hero-image'
-                  alt="hero poster" />
+                  src={
+                    songs.snippet.thumbnails?.maxres
+                      ? songs.snippet?.thumbnails.maxres?.url
+                      : songs.snippet?.thumbnails.high?.url
+                  }
+                  className="hero-image"
+                  alt="hero poster"
+                />
               </div>
-              <div className="image-shadow">
-
-              </div>
+              <div className="image-shadow"></div>
             </div>
-          )}
+          ))}
         </Slider>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
