1c3a99a872c5b1caaee213d8b72782f57fea2971
onam <onamkrverma@gmail.com>
2023-03-21 13:38:59 +0530

    Change search title

--- src/page/searchResult/SearchResult.js
+++ src/page/searchResult/SearchResult.js
@@ -1,41 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
-import Header from '../../components/header/Header';
-import SongsList from '../../components/songsList/SongsList';
-import { useGetSearchItemsQuery } from '../../reduxtool/services/songsApi';
-import Player from '../player/Player';
-import './SearchResult.css'
-
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Header from "../../components/header/Header";
+import SongsList from "../../components/songsList/SongsList";
+import { useGetSearchItemsQuery } from "../../reduxtool/services/songsApi";
+import Player from "../player/Player";
+import "./SearchResult.css";
 
 const SearchResult = () => {
-  const { q } = useParams()
-  const { data,isLoading } = useGetSearchItemsQuery(q)
-  const currentSong = useSelector((state) => state.currentSongSlice.currentSongInfo)
+  const { q } = useParams();
+  const { data, isLoading } = useGetSearchItemsQuery(q);
+  const currentSong = useSelector(
+    (state) => state.currentSongSlice.currentSongInfo
+  );
   const { id } = currentSong;
 
-  const [searchResult, setSearchResult] = useState({})
+  const [searchResult, setSearchResult] = useState({});
 
   // console.log(q)
   // console.log(searchResult)
 
-  
-
   useEffect(() => {
     if (data) {
-      setSearchResult(data)
+      setSearchResult(data);
     }
-  }, [data])
-
-
+  }, [data]);
 
   return (
-    <div className='search-result-container '>
+    <div className="search-result-container ">
       <Header />
-      <SongsList title={'Search result'} songsData={searchResult?.items} searchResult={'searchResult'} isLoading={isLoading} />
+      <SongsList
+        title={"Search results"}
+        songsData={searchResult?.items}
+        searchResult={"searchResult"}
+        isLoading={isLoading}
+      />
       {id && <Player />}
     </div>
-  )
-}
+  );
+};
 
-export default SearchResult
+export default SearchResult;
