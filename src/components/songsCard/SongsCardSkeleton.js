950e034ae55394c3ae715fe288545a2de847da0c
onam <onamkrverma@gmail.com>
2023-03-21 13:30:53 +0530

    Change skeleton theme

--- src/components/songsCard/SongsCardSkeleton.js
+++ src/components/songsCard/SongsCardSkeleton.js
@@ -1,31 +1,26 @@
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const SongsCardSkeleton = ({amount}) => {
+const SongsCardSkeleton = ({ amount }) => {
+  const arrayCount = Array(amount).fill(1);
 
-  const arrayCount = Array(amount).fill(1)
-
-
-
-  return (
-    arrayCount.map((val,index)=>
-    <div className='songs-card-container cur-pointer' key={index}>
+  return arrayCount.map((val, index) => (
+    <div className="songs-card-container cur-pointer" key={index}>
       <div className="songs-card-wrapper">
-        <div className="songs-image-wrapper">
-         <Skeleton height={'100%'}/>
-        </div>
-        <div className="songs-title-wrapper">
-          <p className="songs-title">
-            <Skeleton height={'8px'} count={2}/>
-          </p>
-        </div>
+        <SkeletonTheme baseColor="#202020" highlightColor="#292828">
+          <div className="songs-image-wrapper">
+            <Skeleton height={"100%"} />
+          </div>
+          <div className="songs-title-wrapper">
+            <p className="songs-title">
+              <Skeleton height={"8px"} count={2} />
+            </p>
+          </div>
+        </SkeletonTheme>
       </div>
-
     </div>
-    )
-    
-  )
-}
+  ));
+};
 
-export default SongsCardSkeleton
+export default SongsCardSkeleton;
--- src/page/home/heroBanner/HeroBannerSkeleton.js
+++ src/page/home/heroBanner/HeroBannerSkeleton.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HeroBannerSkeleton = () => {
   return (
-      <div className="heroBanner-wrapper " >
+    <div className="heroBanner-wrapper ">
+      <SkeletonTheme baseColor="#202020" highlightColor="#292828">
         <div className="hero-image-wrapper">
-          <Skeleton height={'100%'}/>
+          <Skeleton height={"100%"} />
         </div>
-      </div>
-  )
-}
+      </SkeletonTheme>
+    </div>
+  );
+};
 
-export default HeroBannerSkeleton
+export default HeroBannerSkeleton;
