c130d0b1dbcd56860016810155f66609c3c5478e
onam <onamkrverma@gmail.com>
2024-03-19 17:41:37 +0530

    update custom PWA with vitePwa plugin

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./reduxtool/store";
-import * as serviceWorkerRegistration from "../serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,8 +13,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), VitePWA({ registerType: "autoUpdate" })],
   build: {
     outDir: "build",
   },
